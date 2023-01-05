34521b7e74f5d5b167c1285ebc4ea4ac192a6519
cchanto <35463225+cchanto@users.noreply.github.com>
2023-01-04 19:54:49 -0600

    Update alb.tf
    adding the right one

--- infra-ecs/alb.tf
+++ infra-ecs/alb.tf
@@ -1,5 +1,7 @@
+
 resource "aws_alb" "alb" {
   name               = var.project_name
+  internal           =    false
   load_balancer_type = "application"
   subnets            = [aws_subnet.subnet_1.id, aws_subnet.subnet_2.id]
   security_groups    = [aws_security_group.alb.id]
@@ -10,28 +12,21 @@ resource "aws_alb" "alb" {
 }
 
 resource "aws_alb_target_group" "alb_target_group" {
-  name        = var.project_name
-  port        = 80
+  name        = var.project_name 
+  port        = 8080   #targest groups
   protocol    = "HTTP"
   vpc_id      = aws_vpc.vpc.id
   target_type = "ip"
-  #  health_check {
-  #   path = "/health"
-  #   port = 80
-  #   healthy_threshold = 6
-  #   unhealthy_threshold = 2
-  #   timeout = 2
-  #   interval = 5
-  #   matcher = "200"  # has to be HTTP 200 or fails
-  # }
 }
 
 resource "aws_alb_listener" "alb_listener" {
   load_balancer_arn = aws_alb.alb.arn
-  port              = 8080
+  #vpc_id      = aws_vpc.vpc.id
+  port              = 80 #loadblancer port
   protocol          = "HTTP"
   default_action {
     target_group_arn = aws_alb_target_group.alb_target_group.arn
     type             = "forward"
   }
 }
+
